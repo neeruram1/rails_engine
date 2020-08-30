02d831a706e2f210b0fb6b1759e3dc6fa830000d
“Neeru <neeram85@gmail.com>
2020-08-30 09:57:22 -0600

    Create Invoice model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,6 @@
 class Merchant < ApplicationRecord
   validates :name, presence: true
   has_many :items
+  has_many :invoices
+  has_many :customers, through: :invoices
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -7,5 +7,7 @@ RSpec.describe Merchant, type: :model do
 
   describe 'Relationships' do
     it {should have_many :items}
+    it {should have_many :invoices}
+    it {should have_many(:customers).through(:invoices)}
   end
 end
