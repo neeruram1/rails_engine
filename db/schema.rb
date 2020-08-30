80e3b28a939bfb2b1f0fd589025b064f8fc4af0e
“Neeru <neeram85@gmail.com>
2020-08-30 09:37:10 -0600

    Create Item model with validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_30_152403) do
+ActiveRecord::Schema.define(version: 2020_08_30_153518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.float "unit_price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "merchants", force: :cascade do |t|
     t.string "name"
