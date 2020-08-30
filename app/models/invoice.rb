5a7c3acfeb44f33bf5f89724f74e6fa29f2e39a9
“Neeru <neeram85@gmail.com>
2020-08-30 10:13:31 -0600

    Added invoice_items relationships to invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,4 +2,6 @@ class Invoice < ApplicationRecord
   validates :status, presence: true
   belongs_to :merchant
   belongs_to :customer
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -8,5 +8,7 @@ RSpec.describe Invoice, type: :model do
   describe 'Relationships' do
     it {should belong_to :merchant}
     it {should belong_to :customer}
+    it {should have_many :invoice_items}
+    it {should have_many(:items).through(:invoice_items)}
   end
 end
