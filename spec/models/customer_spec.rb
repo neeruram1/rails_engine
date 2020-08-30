66327128926e3c264aea69fb19380f1367c87f62
“Neeru <neeram85@gmail.com>
2020-08-30 10:02:06 -0600

    Customer has many invoices

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Customer, type: :model do
     it {should validate_presence_of :first_name}
     it {should validate_presence_of :last_name}
   end
+
+  describe 'Relationships' do
+    it {should have_many :invoices}
+    it {should have_many(:merchants).through(:invoices)}
+  end
 end
