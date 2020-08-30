f89c0e34cede487ee92bd613bfa5e3fdc95818a3
“Neeru <neeram85@gmail.com>
2020-08-30 10:22:17 -0600

    Added relationship validations for all models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_30_160644) do
+ActiveRecord::Schema.define(version: 2020_08_30_161713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,8 @@ ActiveRecord::Schema.define(version: 2020_08_30_160644) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string "result"
+    t.bigint "invoice_id"
+    t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
   end
 
   add_foreign_key "invoice_items", "invoices"
@@ -73,4 +75,5 @@ ActiveRecord::Schema.define(version: 2020_08_30_160644) do
   add_foreign_key "invoices", "customers"
   add_foreign_key "invoices", "merchants"
   add_foreign_key "items", "merchants"
+  add_foreign_key "transactions", "invoices"
 end
