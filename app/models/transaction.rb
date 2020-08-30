c28b4942bc110dc7f73e0b05bb8331a5942eab8b
“Neeru <neeram85@gmail.com>
2020-08-30 09:14:11 -0600

    Create Transactions table in db

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,2 @@
 class Transaction < ApplicationRecord
-end 
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_08_30_150824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "transactions", force: :cascade do |t|
+    t.integer "credit_card_number"
+    t.string "credit_card_expiration_date"
+    t.string "date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
