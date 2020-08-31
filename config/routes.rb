a52c55517bd6e844369b0361bb47ee9960d14a9d
“Neeru <neeram85@gmail.com>
2020-08-31 12:44:29 -0600

    Item can find associated merchant endpoint done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      namespace :items do
+        get "/:id/merchant", to: 'merchant#show'
+      end
       resources :items, only: [:index, :show, :create, :update, :destroy]
       resources :merchants, only: [:index, :show, :create, :update, :destroy]
     end
