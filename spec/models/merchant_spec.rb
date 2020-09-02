1e0f402d2b3870036a962906298c234123e38668
“Neeru <neeram85@gmail.com>
2020-09-02 17:56:11 -0600

    added test for most_items_sold model method

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -72,5 +72,30 @@ RSpec.describe Merchant, type: :model do
 
         expect(Merchant.most_revenue(param)).to eq([merchant1, merchant2])
       end
+
+      it ".most_items_sold" do
+        merchant1 = create(:merchant)
+        merchant2 = create(:merchant)
+        merchant3 = create(:merchant)
+        customer1 = create(:customer)
+        item1 = create(:item, merchant: merchant1)
+        item2 = create(:item, merchant: merchant2)
+        item3 = create(:item, merchant: merchant3)
+        item4 = create(:item, merchant: merchant3)
+        invoice1 = create(:invoice, customer: customer1, merchant: merchant1)
+        invoice2 = create(:invoice, customer: customer1, merchant: merchant2)
+        invoice3 = create(:invoice, customer: customer1, merchant: merchant3)
+        invoice_item1 = create(:invoice_item, invoice: invoice1, item: item1, quantity: 40)
+        invoice_item2 = create(:invoice_item, invoice: invoice2, item: item2, quantity: 100)
+        invoice_item3 = create(:invoice_item, invoice: invoice3, item: item2, quantity: 27)
+        invoice_item4 = create(:invoice_item, invoice: invoice1, item: item3, quantity: 12)
+        transaction1 = create(:transaction, invoice: invoice1)
+        transaction2 = create(:transaction, invoice: invoice2)
+        transaction3 = create(:transaction, invoice: invoice3)
+
+        param = 3
+
+        expect(Merchant.most_items_sold(param)).to eq([merchant2, merchant1, merchant3])
+      end
     end
   end
