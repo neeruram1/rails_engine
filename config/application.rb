dec9dc6e6f36f8c64230db87d99ef646b162b6a7
“Neeru <neeram85@gmail.com>
2020-09-02 07:15:03 -0600

    remove active storage routes

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module RailsEngine
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    initializer(:remove_activestorage_routes, after: :add_routing_paths) {|app|
+      app.routes_reloader.paths.delete_if {|path| path =~ /activestorage/}}
   end
 end
