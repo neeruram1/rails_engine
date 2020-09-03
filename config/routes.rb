0c2d85c7fa71b820c75f9c660367799e9e7bcf2e
“Neeru <neeram85@gmail.com>
2020-09-02 18:23:50 -0600

    Finished most items sold endpoint tests passing

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,28 @@ Rails.application.routes.draw do
     namespace :v1 do
 
       namespace :items do
+        #item's merchant
         get "/:id/merchant", to: 'merchant#show'
+
+        #search by attribute
         get "/find", to: 'search#show'
         get "/find_all", to: 'search#index'
       end
 
       namespace :merchants do
+        #merchant items
         get "/:id/items", to: 'items#index'
+
+        #search by attribute
         get "/find", to: 'search#show'
         get "/find_all", to: 'search#index'
+
+        #revenue
         get "/most_revenue", to: 'revenue#index'
         get "/:id/revenue", to: 'revenue#show'
+
+        #most items sold
+        get "/most_items", to: 'items_sold#index'
       end
 
       resources :items, only: [:index, :show, :create, :update, :destroy]
