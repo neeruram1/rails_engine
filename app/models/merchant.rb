a18b9ffecdb101c6bafc3ffcb40f87a1dcfd100b
“Neeru <neeram85@gmail.com>
2020-09-02 18:49:41 -0600

    wrote test for merchant class method revenue_by_date_range

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -6,24 +6,24 @@ class Merchant < ApplicationRecord
   has_many :invoice_items, through: :invoices
   has_many :transactions, through: :invoices
 
-  scope :find_by_date, -> (param) {where("to_char(#{param.keys.first},'yyyy-mon-dd-HH-MI-SS') ILIKE ?", "%#{param.values.first}%")}
-  scope :find_by_attribute, -> (param) {where("merchants.#{param.keys.first}::text ILIKE ?", "%#{param.values.first}%")}
+  scope :by_date, -> (param) {where("to_char(#{param.keys.first},'yyyy-mon-dd-HH-MI-SS') ILIKE ?", "%#{param.values.first}%")}
+  scope :by_attribute, -> (param) {where("merchants.#{param.keys.first}::text ILIKE ?", "%#{param.values.first}%")}
 
   def self.find_merchant(param)
     attribute = param.keys.first
     if attribute == 'created_at' || attribute == 'updated_at'
-      find_by_date(param).first
+      by_date(param).first
     else
-      find_by_attribute(param).first
+      by_attribute(param).first
     end
   end
 
   def self.find_all_merchants(param)
     attribute = param.keys.first
     if attribute == 'created_at' || attribute == 'updated_at'
-      find_by_date(param)
+      by_date(param)
     else
-      find_by_attribute(param)
+      by_attribute(param)
     end
   end
 
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Merchant, type: :model do
   end
 
   describe 'Methods' do
-      it ".find_merchant()" do
+      it "find_merchant()" do
         merchant1 = create(:merchant, name: 'The Bluth Company')
         merchant2 = create(:merchant, name: 'Dunder Mifflin Paper Company')
         param = {"name"=>"blu"}
@@ -24,23 +24,23 @@ RSpec.describe Merchant, type: :model do
       expect(Merchant.find_merchant(param1)).to eq(merchant2)
     end
 
-    it ".find_all_merchants()" do
+    it "find_all_merchants()" do
       merchant1 = create_list(:merchant, 34, name: 'The Bluth Company')
       param = {"name"=>"blu"}
 
       expect(Merchant.find_all_merchants(param).count).to eq(34)
     end
 
-      it ".total_revenue" do
-        merchant = Merchant.create!(name: "The Bluth Company")
+      it "total_revenue" do
+        merchant = create(:merchant)
         item = Item.create!(name: "Banana", unit_price: 10.75, description: "A banana", merchant: merchant)
         item2 = Item.create!(name: "Chocolate Frog", unit_price: 4.75, description: "A chocolate frog", merchant: merchant)
         customer = Customer.create!(first_name: "Michael", last_name: "Bluth")
         customer2 = Customer.create!(first_name: "Job", last_name: "Bluth")
         invoice = Invoice.create!(customer: customer, merchant: merchant, status: 'shipped')
         invoice2 = Invoice.create!(customer: customer2, merchant: merchant, status: 'shipped')
-        invoice_item = InvoiceItem.create!(quantity: 30, unit_price: item.unit_price, item: item, invoice: invoice)
-        invoice_item2 = InvoiceItem.create!(quantity: 14, unit_price: item2.unit_price, item: item2, invoice: invoice2)
+        invoice_item = InvoiceItem.create!(quantity: 30, unit_price: 10.75, item: item, invoice: invoice)
+        invoice_item2 = InvoiceItem.create!(quantity: 14, unit_price: 4.75, item: item2, invoice: invoice2)
         transaction = Transaction.create!(credit_card_number: 123, result: "success", invoice: invoice)
         transaction1 = Transaction.create!(credit_card_number: 123, result: "failure", invoice: invoice)
         transaction2 = Transaction.create!(credit_card_number: 123, result: "success", invoice: invoice2)
@@ -49,7 +49,7 @@ RSpec.describe Merchant, type: :model do
         expect((10.75 * 30) + (4.75 * 14)).to eq(389)
       end
 
-      it ".most_revenue()" do
+      it "most_revenue()" do
         merchant1 = create(:merchant)
         merchant2 = create(:merchant)
         customer1 = create(:customer)
@@ -73,7 +73,7 @@ RSpec.describe Merchant, type: :model do
         expect(Merchant.most_revenue(param)).to eq([merchant1, merchant2])
       end
 
-      it ".most_items_sold" do
+      it "most_items_sold" do
         merchant1 = create(:merchant)
         merchant2 = create(:merchant)
         merchant3 = create(:merchant)
@@ -96,6 +96,28 @@ RSpec.describe Merchant, type: :model do
         param = 3
 
         expect(Merchant.most_items_sold(param)).to eq([merchant2, merchant1, merchant3])
+      end
+
+      it "revenue_by_date_range()" do
+        merchant1 = create(:merchant)
+        merchant2 = create(:merchant)
+        customer1 = create(:customer)
+        item1 = create(:item, merchant: merchant1)
+        item2 = create(:item, merchant: merchant2)
+        invoice1 = create(:invoice, customer: customer1, merchant: merchant1, created_at: "'Fri, 04 Sep 2020 05:00:00 UTC +00:00'")
+        invoice2 = create(:invoice, customer: customer1, merchant: merchant1, created_at: "'Thu, 03 Sep 2020 05:10:00 UTC +00:00'")
+        invoice3 = create(:invoice, customer: customer1, merchant: merchant2, created_at: "'Wed, 28 Apr 2010 05:00:00 UTC +00:00'")
+        invoice4 = create(:invoice, customer: customer1, merchant: merchant2, created_at: "'Fri, 04 Sep 2020 07:00:00 UTC +00:00'")
+        invoice_item1 = create(:invoice_item, invoice: invoice1, item: item1, unit_price: 45.78, quantity: 40)
+        invoice_item2 = create(:invoice_item, invoice: invoice2, item: item1, unit_price: 45.78, quantity: 100)
+        invoice_item3 = create(:invoice_item, invoice: invoice3, item: item2, unit_price: 63.50, quantity: 27)
+        invoice_item4 = create(:invoice_item, invoice: invoice4, item: item2, unit_price: 34.00, quantity: 12)
+        transaction1 = create(:transaction, invoice: invoice1)
+        transaction2 = create(:transaction, invoice: invoice2)
+        transaction3 = create(:transaction, invoice: invoice3)
+        transaction4 = create(:transaction, invoice: invoice4)
+
+        expect(Merchant.revenue_by_date_range("2020-09-03", "2020-09-04")).to eq(6817.2)
       end
     end
   end
