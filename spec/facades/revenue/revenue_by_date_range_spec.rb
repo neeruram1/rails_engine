b1aa213a9713a9488f4a35f082085044d7447840
“Neeru <neeram85@gmail.com>
2020-09-02 18:53:10 -0600

    Move revenue_by_date_range to revenue facade

---
+++ spec/facades/revenue/revenue_by_date_range_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+describe 'Revenue facade methods' do
+  it "revenue_by_date_range()" do
+    merchant1 = create(:merchant)
+    merchant2 = create(:merchant)
+    customer1 = create(:customer)
+    item1 = create(:item, merchant: merchant1)
+    item2 = create(:item, merchant: merchant2)
+    invoice1 = create(:invoice, customer: customer1, merchant: merchant1, created_at: "'Fri, 04 Sep 2020 05:00:00 UTC +00:00'")
+    invoice2 = create(:invoice, customer: customer1, merchant: merchant1, created_at: "'Thu, 03 Sep 2020 05:10:00 UTC +00:00'")
+    invoice3 = create(:invoice, customer: customer1, merchant: merchant2, created_at: "'Wed, 28 Apr 2010 05:00:00 UTC +00:00'")
+    invoice4 = create(:invoice, customer: customer1, merchant: merchant2, created_at: "'Fri, 04 Sep 2020 07:00:00 UTC +00:00'")
+    invoice_item1 = create(:invoice_item, invoice: invoice1, item: item1, unit_price: 45.78, quantity: 40)
+    invoice_item2 = create(:invoice_item, invoice: invoice2, item: item1, unit_price: 45.78, quantity: 100)
+    invoice_item3 = create(:invoice_item, invoice: invoice3, item: item2, unit_price: 63.50, quantity: 27)
+    invoice_item4 = create(:invoice_item, invoice: invoice4, item: item2, unit_price: 34.00, quantity: 12)
+    transaction1 = create(:transaction, invoice: invoice1)
+    transaction2 = create(:transaction, invoice: invoice2)
+    transaction3 = create(:transaction, invoice: invoice3)
+    transaction4 = create(:transaction, invoice: invoice4)
+
+    expect(RevenueFacade.revenue_by_date_range("2020-09-03", "2020-09-04")).to eq(6817.2)
+  end
+end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -97,27 +97,5 @@ RSpec.describe Merchant, type: :model do
 
         expect(Merchant.most_items_sold(param)).to eq([merchant2, merchant1, merchant3])
       end
-
-      it "revenue_by_date_range()" do
-        merchant1 = create(:merchant)
-        merchant2 = create(:merchant)
-        customer1 = create(:customer)
-        item1 = create(:item, merchant: merchant1)
-        item2 = create(:item, merchant: merchant2)
-        invoice1 = create(:invoice, customer: customer1, merchant: merchant1, created_at: "'Fri, 04 Sep 2020 05:00:00 UTC +00:00'")
-        invoice2 = create(:invoice, customer: customer1, merchant: merchant1, created_at: "'Thu, 03 Sep 2020 05:10:00 UTC +00:00'")
-        invoice3 = create(:invoice, customer: customer1, merchant: merchant2, created_at: "'Wed, 28 Apr 2010 05:00:00 UTC +00:00'")
-        invoice4 = create(:invoice, customer: customer1, merchant: merchant2, created_at: "'Fri, 04 Sep 2020 07:00:00 UTC +00:00'")
-        invoice_item1 = create(:invoice_item, invoice: invoice1, item: item1, unit_price: 45.78, quantity: 40)
-        invoice_item2 = create(:invoice_item, invoice: invoice2, item: item1, unit_price: 45.78, quantity: 100)
-        invoice_item3 = create(:invoice_item, invoice: invoice3, item: item2, unit_price: 63.50, quantity: 27)
-        invoice_item4 = create(:invoice_item, invoice: invoice4, item: item2, unit_price: 34.00, quantity: 12)
-        transaction1 = create(:transaction, invoice: invoice1)
-        transaction2 = create(:transaction, invoice: invoice2)
-        transaction3 = create(:transaction, invoice: invoice3)
-        transaction4 = create(:transaction, invoice: invoice4)
-
-        expect(Merchant.revenue_by_date_range("2020-09-03", "2020-09-04")).to eq(6817.2)
-      end
     end
   end
